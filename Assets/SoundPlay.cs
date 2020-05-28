f76c797e5528b6b70d777f2aed34a7b6e996fd59
Evan O'Hara <evan_ohara@yahoo.com>
2020-05-28 15:58:06 -0700

    Added sounds.

---
+++ Assets/SoundPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlay : MonoBehaviour
+{
+    AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayOrPause()
+    {
+        if (source.isPlaying)
+            source.Stop();
+        else
+            source.Play();
+    }
+}
